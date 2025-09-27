c21870cbdd92758111bcd2388cc6506691231547
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-09-26 23:27:33 -0500

    person service created

--- src/services/cruds/Person_Service.py
+++ src/services/cruds/Person_Service.py
@@ -1,17 +1,44 @@
 from interfaces import Crud_Interface
+from src.models.cruds.Person_Model import Person_Model
+from src.database import get_db_conecction
 
 class Person_Service(Crud_Interface):
-    def __init__(self, person_model):
-        self.person_model = person_model
-
-    def create(self, data):
+    
+    # Implementing the abstract methods from Crud_Interface
+    # Consult all persons
+    @classmethod
+    def consult(cls):
+        # Implement the logic to retrieve all persons from the database
+        try:
+            connection = get_db_conecction()
+            people = []
+        
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM Person")
+                result_person = cursor.fetchall()
+                for row in result_person:
+                    person = Person_Model(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+                    people.append(person.to_dict())
+                connection.commit()
+            return people
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            connection.close()
+    
+    # Create a new person
+    @classmethod
+    def create(cls, data):
+        pass
+    
+    @classmethod
+    def consult_id(cls, id):
         pass
 
-    def read(self, id):
+    @classmethod
+    def update(cls, id, data):
         pass
 
-    def update(self, id, data):
-        pass
-
-    def change_state(self, id):
+    @classmethod
+    def change_state(cls, id):
         pass
