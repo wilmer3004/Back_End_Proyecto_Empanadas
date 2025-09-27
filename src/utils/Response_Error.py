3c84c776bf4fb50a4b6a1a8ef4878f2f41af56a9
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-09-27 14:30:49 -0500

    Update Response_Error.py

--- src/utils/Response_Error.py
+++ src/utils/Response_Error.py
@@ -1,8 +1,12 @@
-class Response_Error(Exception):
-    def __init__(self, message, status_code=400):
-        super().__init__(message)
-        self.status_code = status_code
+# src/utils/Response_Error.py
+class Response_Error:
+    def __init__(self, message, code=500):
         self.message = message
-    
+        self.code = code
+
     def to_dict(self):
-        return {"error": self.message, "code": self.status_code}
+        return {
+            "success": False,
+            "error": self.message,
+            "code": self.code
+        }
