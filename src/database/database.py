86115140ac3bb0d194149fdf2e6b56c0f30d49c1
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-09-27 14:11:28 -0500

    Error fix, where it affected an unauthorized parsing

--- src/database/database.py
+++ src/database/database.py
@@ -3,14 +3,12 @@ import psycopg2
 
 def get_db_conecction():
     try:
-        # Use the config function to get the enviroment variables
         connection = psycopg2.connect(
-            host=config('POSTGRES_HOST'),
-            user=config('POSTGRES_USER'),
-            password=config('POSTGRES_PASSWORD'),
-            dbname=config('POSTGRES_DB'),
-            port=int(config('POSTGRES_PORT'), default = 5432),
-
+            host=config('POSTGRES_HOST', default='localhost'),
+            user=config('POSTGRES_USER', default='postgres'),
+            password=config('POSTGRES_PASSWORD', default='admin'),
+            dbname=config('POSTGRES_DB', default='empanadas_db'),
+            port=config('POSTGRES_PORT', default=5432, cast=int)
         )
         return connection
     except Exception as e:
