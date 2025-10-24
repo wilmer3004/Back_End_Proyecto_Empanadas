60d98849cebef9796fdcf9730be95b755211c73d
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-10-24 11:09:20 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from src.routes.cruds.Person_Route import Person_Route
 from src.routes.cruds.Document_Type_Route import Document_Type_Route
 from src.routes.cruds.Customer_Type_Route import Customer_Type_Route
 from src.routes.cruds.Report_Type_Route import Report_Type_Route
+from src.routes.cruds.Product_Type_Route import Product_Type_Route
 
 app = Flask(__name__)
 
@@ -19,5 +20,7 @@ def init_app(config):
     app.register_blueprint(Customer_Type_Route.main, url_prefix='/api/v1/customer_types') 
     # Register the Report_Type_Route blueprint with a URL prefix
     app.register_blueprint(Report_Type_Route.main, url_prefix='/api/v1/report_types')
+    # Register the Product_Type_Route blueprint with a URL prefix
+    app.register_blueprint(Product_Type_Route.main, url_prefix='/api/v1/product_types')
     
     return app
