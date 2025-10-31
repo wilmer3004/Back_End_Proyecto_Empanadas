f371c3e31e58714755b0c11544ac38452607f5af
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-10-31 10:41:49 -0500

    Update Order_Product_Model.py

--- src/models/cruds/Order_Product_Model.py
+++ src/models/cruds/Order_Product_Model.py
@@ -1,9 +1,10 @@
 class Order_Product_Model:
     
     # Constructor
-    def __init__(self, id_order_prod_fk, id_product_fk):
+    def __init__(self, id_order_prod_fk, id_product_fk, quantity_product = 0):
         self.__id_order_prod_fk = id_order_prod_fk
         self.__id_product_fk = id_product_fk
+        self.__quantity_product = quantity_product
     
     # Getters
     def get_id_order_prod_fk(self):
@@ -12,6 +13,9 @@ class Order_Product_Model:
     def get_id_product_fk(self):
         return self.__id_product_fk
     
+    def get_quantity_product(self):
+        return self.__quantity_product
+    
     # Setters
     def set_id_order_prod_fk(self, id_order_prod_fk):
         self.__id_order_prod_fk = id_order_prod_fk
@@ -19,10 +23,14 @@ class Order_Product_Model:
     def set_id_product_fk(self, id_product_fk):
         self.__id_product_fk = id_product_fk
     
+    def set_quantity_product(self, quantity_product):
+        self.__quantity_product = quantity_product
+    
     # Method to convert the object to a dictionary
     def to_dict(self):
         return {
             "id_order_prod_fk": self.__id_order_prod_fk,
-            "id_product_fk": self.__id_product_fk
+            "id_product_fk": self.__id_product_fk,
+            "quantity_product": self.__quantity_product
         }
     
