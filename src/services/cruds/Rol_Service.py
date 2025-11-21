7842232bf262d02519cfdca3d904bc62ffd5a9fa
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-11-21 01:56:04 -0500

    Refactor Rol_Service methods for improved readability and fix SQL query errors

--- src/services/cruds/Rol_Service.py
+++ src/services/cruds/Rol_Service.py
@@ -44,68 +44,69 @@ class Rol_Service(Crud_Interface):
         finally:
             connection.close()
             
-# Consult a rol by ID
-@classmethod
-def consult_id(cls,id):
-    try:
-        connection = get_db_conecction()
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM rol WHERE id_rol = %s"
-            cursor.execute(sql, (id,))
-            row = cursor.fetchone()
-            if row:
-                rol = Rol_Model(row[0], row[1], row [2])
-                return rol.to_dict()
-            else:
-                return{"error": "Rol not found", "code": 404}
-    except  Exception as e:
-        print(f"An error ocurred: {e}")
-        return {"error": "Failed to retrieve rol", "code": 500}
-    finally:
-        connection.close()
-@classmethod
-def update(cls,id,data):
-    try:
-        connection = get_db_conecction()
-        with connection.cursor() as cursor:
-            sql ="""UPDATE rol set name_rol = %s WHERE id_rol=%s"""
-            data_tuple =(data["name_rol"],id)
-            cursor.excurte(sql,data_tuple)
-            connection.commit()
-            if cursor.rowcount == 0:
-                return {"error": "Rol not found", "code": 404}
-            return {"message" "Rol updated successfuly"}
-    except Exception as e:
-        print(f"An error ocurred: {e}")
-        return {"error": "Failed to update person", "code": 500}
-    finally:
-        connection.close()
+    # Consult a rol by ID
+    @classmethod
+    def consult_id(cls,id):
+        try:
+            connection = get_db_conecction()
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM rol WHERE id_rol = %s"
+                cursor.execute(sql, (id,))
+                row = cursor.fetchone()
+                if row:
+                    rol = Rol_Model(row[0], row[1], row [2])
+                    return rol.to_dict()
+                else:
+                    return{"error": "Rol not found", "code": 404}
+        except  Exception as e:
+            print(f"An error ocurred: {e}")
+            return {"error": "Failed to retrieve rol", "code": 500}
+        finally:
+            connection.close()
+    @classmethod
+    def update(cls,id,data):
+        try:
+            connection = get_db_conecction()
+            with connection.cursor() as cursor:
+                sql ="""UPDATE rol set name_rol = %s WHERE id_rol=%s"""
+                data_tuple =(data["name_rol"],id)
+                cursor.execute(sql,data_tuple)
+                connection.commit()
+                if cursor.rowcount == 0:
+                    return {"error": "Rol not found", "code": 404}
+                return {"message":"Rol updated successfuly"}
+        except Exception as e:
+            print(f"An error ocurred: {e}")
+            return {"error": "Failed to update person", "code": 500}
+        finally:
+            connection.close()
+            
     @classmethod
     def  change_state(cls,id):
         try:
             connection = get_db_conecction()
             with connection.cursor() as cursor:
                 #Firs, check the current state of the role
-                sql_check = "SELECT state_rol FROM person WHERE id_rol = %s"
+                sql_check = "SELECT state_rol FROM rol WHERE id_rol = %s"
                 cursor.execute(sql_check,(id,))
-                row = cursor. fetchone()
+                row = cursor.fetchone()
                 if not row:
                     return{"error": "Rol not found", "code": 404}
                 
                 current_state = row[0]
                 new_state = not current_state
                 
-            # Update the state
-            sql_update = "UPDATE rol SET state_rol = %s WHERE id_rol =%s"
-            cursor.execute(sql_update, (new_state,id))
-            connection.commit()
+                # Update the state
+                sql_update = "UPDATE rol SET state_rol = %s WHERE id_rol =%s"
+                cursor.execute(sql_update, (new_state,id))
+                connection.commit()
             
             return {"message": "Rol state changed successfully", "new_state": new_state}
         except Exception as e:
-            print(f"An error ocurred: (e)")
+            print(f"An error ocurred: {e}")
             return {"error": "Failed to change state_rol", "code": 500}
         finally:
             connection.close()
-    
-            
+
                 
+                    
