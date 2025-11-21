7ba59ca84036221398de4ce9ccf2e9594529eba0
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2025-11-20 22:18:45 -0500

    Create Auth_Model.py

---
+++ src/security/model/Auth_Model.py
@@ -0,0 +1,20 @@
+class Auth_Model:
+    def __init__(self, id_user, id_rol_fk, id_person_fk, user_name, password_user, token_user, state_user):
+        self.__id_user = id_user
+        self.__id_rol_fk = id_rol_fk
+        self.__id_person_fk = id_person_fk
+        self.__user_name = user_name
+        self.__password_user = password_user
+        self.__token_user = token_user
+        self.__state_user = state_user
+
+    def to_dict(self):
+        return {
+            "id_user": self.__id_user,
+            "id_rol_fk": self.__id_rol_fk,
+            "id_person_fk": self.__id_person_fk,
+            "user_name": self.__user_name,
+            "password_user": self.__password_user,
+            "token_user": self.__token_user,
+            "state_user": self.__state_user
+        }
